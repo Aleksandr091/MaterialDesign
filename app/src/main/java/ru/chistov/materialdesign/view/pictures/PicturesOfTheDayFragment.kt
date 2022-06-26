4af62d4b34e2151d7aded4e513dbd2dff2ee9bd8
Александр <chist675@gmail.com>
2022-06-26 17:23:18 +0300

    Дз 4-го урока,Не работает MotionLayout, поправил viewPager

--- app/src/main/java/ru/chistov/materialdesign/view/pictures/PicturesOfTheDayFragment.kt
+++ app/src/main/java/ru/chistov/materialdesign/view/pictures/PicturesOfTheDayFragment.kt
@@ -117,118 +117,118 @@ class PicturesOfTheDayFragment : Fragment() {
         })
     }
 
-    /*private fun fabListener() {
-        binding.fab.setOnClickListener {
-            if (isMain) {
-                binding.bottomAppBar.navigationIcon = null
-                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
-                binding.fab.setImageDrawable(
-                    ContextCompat.getDrawable(
-                        requireContext(),
-                        R.drawable.ic_back_fab
-                    )
-                )
-                binding.bottomAppBar.replaceMenu(R.menu.menu_about)
-            } else {
-                binding.bottomAppBar.navigationIcon = ContextCompat.getDrawable(
+/*private fun fabListener() {
+    binding.fab.setOnClickListener {
+        if (isMain) {
+            binding.bottomAppBar.navigationIcon = null
+            binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+            binding.fab.setImageDrawable(
+                ContextCompat.getDrawable(
                     requireContext(),
-                    R.drawable.ic_hamburger_menu_bottom_bar
+                    R.drawable.ic_back_fab
                 )
-                binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
-                binding.fab.setImageDrawable(
-                    ContextCompat.getDrawable(
-                        requireContext(),
-                        R.drawable.ic_plus_fab
-                    )
-                )
-                binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_bar)
-            }
-            isMain = !isMain
-        }
-    }*/
-
-    /*private fun initBottomSheetBehavior() {
-        val bottomSheetBehavior = BottomSheetBehavior.from(binding.lifeHack.bottomSheetContainer)
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
-    }*/
-
-    private fun setClickListenerTextInputLayout() {
-        binding.inputLayout.setEndIconOnClickListener {
-            startActivity(Intent(Intent.ACTION_VIEW).apply {
-                data =
-                    Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
-            })
-        }
-    }
-
-    /*private fun initMenu() {
-        (requireActivity() as MainActivity).setSupportActionBar(binding.bottomAppBar)
-        setHasOptionsMenu(true)
-    }*/
-
-
-    private fun renderData(picturesOfTheDayAppState: PicturesOfTheDayAppState) {
-        when (picturesOfTheDayAppState) {
-            is PicturesOfTheDayAppState.Error -> {
-                Snackbar.make(
-                    requireContext(),
-                    binding.root,
-                    picturesOfTheDayAppState.message,
-                    Snackbar.LENGTH_LONG
-                ).show()
-
-            }
-            is PicturesOfTheDayAppState.Loading -> {
-                binding.imageView.load(R.drawable.progress_animation) {
-                    error(R.drawable.ic_vector_load_error)
-                }
-            }
-            is PicturesOfTheDayAppState.Success -> {
-                setData(picturesOfTheDayAppState)
-            }
-        }
-    }
-
-
-    private fun setData(data: PicturesOfTheDayAppState.Success) {
-        val url = data.pictureOfTheDayResponseData.hdurl
-        if (url.isNullOrEmpty()) {
-            val videoUrl = data.pictureOfTheDayResponseData.url
-            videoUrl?.let { showAVideoUrl(it) }
+            )
+            binding.bottomAppBar.replaceMenu(R.menu.menu_about)
         } else {
-            binding.imageView.load(data.pictureOfTheDayResponseData.hdurl) {
-                this.placeholder(R.drawable.progress_animation)
-                crossfade(true)
+            binding.bottomAppBar.navigationIcon = ContextCompat.getDrawable(
+                requireContext(),
+                R.drawable.ic_hamburger_menu_bottom_bar
+            )
+            binding.bottomAppBar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+            binding.fab.setImageDrawable(
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.ic_plus_fab
+                )
+            )
+            binding.bottomAppBar.replaceMenu(R.menu.menu_bottom_bar)
+        }
+        isMain = !isMain
+    }
+}*/
+
+/*private fun initBottomSheetBehavior() {
+    val bottomSheetBehavior = BottomSheetBehavior.from(binding.lifeHack.bottomSheetContainer)
+    bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+}*/
+
+private fun setClickListenerTextInputLayout() {
+    binding.inputLayout.setEndIconOnClickListener {
+        startActivity(Intent(Intent.ACTION_VIEW).apply {
+            data =
+                Uri.parse("https://en.wikipedia.org/wiki/${binding.inputEditText.text.toString()}")
+        })
+    }
+}
+
+/*private fun initMenu() {
+    (requireActivity() as MainActivity).setSupportActionBar(binding.bottomAppBar)
+    setHasOptionsMenu(true)
+}*/
+
+
+private fun renderData(picturesOfTheDayAppState: PicturesOfTheDayAppState) {
+    when (picturesOfTheDayAppState) {
+        is PicturesOfTheDayAppState.Error -> {
+            Snackbar.make(
+                requireContext(),
+                binding.root,
+                picturesOfTheDayAppState.message,
+                Snackbar.LENGTH_LONG
+            ).show()
+
+        }
+        is PicturesOfTheDayAppState.Loading -> {
+            binding.imageView.load(R.drawable.progress_animation) {
                 error(R.drawable.ic_vector_load_error)
             }
-            binding.title.text =
-                data.pictureOfTheDayResponseData.title
-            binding.explanation.text =
-                data.pictureOfTheDayResponseData.explanation
+        }
+        is PicturesOfTheDayAppState.Success -> {
+            setData(picturesOfTheDayAppState)
         }
     }
+}
 
-    private fun showAVideoUrl(videoUrl: String) = with(binding) {
-        imageView.visibility = View.GONE
-        videoOfTheDay.visibility = View.VISIBLE
-        videoOfTheDay.text = "Сегодня у нас без картинки дня, но есть  видео дня! " +
-                "${videoUrl.toString()} \n кликни >ЗДЕСЬ< чтобы открыть в новом окне"
-        videoOfTheDay.setOnClickListener {
-            val i = Intent(Intent.ACTION_VIEW).apply {
-                data = Uri.parse(videoUrl)
-            }
-            startActivity(i)
+
+private fun setData(data: PicturesOfTheDayAppState.Success) {
+    val url = data.pictureOfTheDayResponseData.hdurl
+    if (url.isNullOrEmpty()) {
+        val videoUrl = data.pictureOfTheDayResponseData.url
+        videoUrl?.let { showAVideoUrl(it) }
+    } else {
+        binding.imageView.load(data.pictureOfTheDayResponseData.hdurl) {
+            this.placeholder(R.drawable.progress_animation)
+            crossfade(true)
+            error(R.drawable.ic_vector_load_error)
         }
+        binding.title.text =
+            data.pictureOfTheDayResponseData.title
+        binding.explanation.text =
+            data.pictureOfTheDayResponseData.explanation
     }
+}
 
-    override fun onDestroy() {
-        _binding = null
-        super.onDestroy()
+private fun showAVideoUrl(videoUrl: String) = with(binding) {
+    imageView.visibility = View.GONE
+    videoOfTheDay.visibility = View.VISIBLE
+    videoOfTheDay.text = "Сегодня у нас без картинки дня, но есть  видео дня! " +
+            "${videoUrl.toString()} \n кликни >ЗДЕСЬ< чтобы открыть в новом окне"
+    videoOfTheDay.setOnClickListener {
+        val i = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(videoUrl)
+        }
+        startActivity(i)
     }
+}
 
-    companion object {
+override fun onDestroy() {
+    _binding = null
+    super.onDestroy()
+}
 
-        @JvmStatic
-        fun newInstance() = PicturesOfTheDayFragment()
-    }
+companion object {
+
+    @JvmStatic
+    fun newInstance() = PicturesOfTheDayFragment()
+}
 }
